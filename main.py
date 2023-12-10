d09b8c261cf52f4403659a58e026bb7fc51ffe64
Amir Hesham Ibrahim <amirhesham65@gmail.com>
2023-12-10 14:36:43 +0200

    Refactor mixer and output panel

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 import pyqtgraph as pg
 
 from models.image_view_port import ImageViewPort
-from models.mixer import OutputPanel
+from models.output_panel import OutputPanel
+from models.mixer import Mixer
 
 uiclass, baseclass = pg.Qt.loadUiType("views/mainwindow.ui")
 
@@ -82,66 +83,12 @@ class MainWindow(uiclass, baseclass):
         for image_view_port in self.images:
             image_view_port.draw_region_square(scale=(value / 100))
 
-        # todo need to change calling place
-        # self._display_mixer_output()
-
-    # todo to be continued
-    def _display_mixer_output(self):
-        img_idx_dictionary = {"Image1": 0, "Image2": 1, "Image3": 2, "Image4": 3}
-
-        image_indices = {
-            "output_1": [
-                img_idx_dictionary[self.image_1_output_1.currentText()],
-                img_idx_dictionary[self.image_2_output_1.currentText()],
-            ],
-            "output_2": [
-                img_idx_dictionary[self.image_1_output_2.currentText()],
-                img_idx_dictionary[self.image_2_output_2.currentText()],
-            ],
-        }
-
-        images = {
-            "output_1": [
-                self.images[image_indices["output_1"][0]].image,
-                self.images[image_indices["output_1"][1]].image,
-            ],
-            "output_2": [
-                self.images[image_indices["output_2"][0]].image,
-                self.images[image_indices["output_2"][1]].image,
-            ],
-        }
-
-        self.image_output_1.clear()
-        self.image_output_2.clear()
-
-        for output in ["output_1", "output_2"]:
-            if all(image is not None for image in images[output]):
-                self._process_images(output, images[output])
-
     def _get_curr_region(self):
         return self.images[0].get_boundries()
 
-    def _process_images(self, output, images):
-        output_idx = int(output.split("_")[-1]) - 1
-        component_1 = self.__getattribute__(f"image_1_component_{output}").currentText()
-        component_2 = self.__getattribute__(f"image_2_component_{output}").currentText()
-
-        if component_1 == "Magnitude" and component_2 == "Phase":
-            self.output_ports[output_idx].reconstruct_image_using_magnitude_phase(
-                *images, region=self._get_curr_region()
-            )
-        elif component_1 == "Phase" and component_2 == "Magnitude":
-            self.output_ports[output_idx].reconstruct_image_using_magnitude_phase(
-                *reversed(images), region=self._get_curr_region()
-            )
-        elif component_1 == "Real" and component_2 == "Imaginary":
-            self.output_ports[output_idx].reconstruct_image_using_real_imaginary(
-                *images, region=self._get_curr_region()
-            )
-        elif component_1 == "Imaginary" and component_2 == "Real":
-            self.output_ports[output_idx].reconstruct_image_using_real_imaginary(
-                *reversed(images), region=self._get_curr_region()
-            )
+    def _display_mixer_output(self):
+        mixer = Mixer(window=self, region=self._get_curr_region())
+        mixer.mix()
 
 
 def main():
--- models/mixer.py
+++ models/mixer.py
@@ -1,141 +1,64 @@
-import cv2
-import numpy as np
-import pyqtgraph as pg
-
-from enum import Enum
-from models.image_view_port import ComponentViewMode
-from models.image import Image
-
-
-class ImageNumber(Enum):
-    """
-    Enum for different component view modes.
-    """
-
-    Image1 = 0
-    Image2 = 1
-    Image3 = 2
-    Image4 = 3
-
-
-class OutputPanel:
-    def __init__(
-        self,
-        window,
-        output_viewer,
-        first_image_combo_box,
-        second_image_combo_box,
-        first_image_mode_compo_box,
-        second_image_mode_compo_box,
-        component1_weight_slider,
-        component2_weight_slider,
-    ):
+class Mixer:
+    def __init__(self, window, region: tuple) -> None:
         self.window = window
-        self.output_viewer = output_viewer
-        self.output_viewer.showAxes(False)
-        self.output_viewer.rotate(90)
-        self.first_image_combo_box = first_image_combo_box
-        self.second_image_combo_box = second_image_combo_box
-        self.first_image_mode_compo_box = first_image_mode_compo_box
-        self.second_image_mode_compo_box = second_image_mode_compo_box
-        self.component_1_weight_slider = component1_weight_slider
-        self.component_2_weight_slider = component2_weight_slider
+        self.region = region
 
-        self.weight_1 = 1
-        self.weight_2 = 1
-        self.image_1_component = ComponentViewMode.MAGNITUDE
-        self.image_2_component = ComponentViewMode.PHASE
+    def mix(self):
+        img_idx_dictionary = {"Image1": 0, "Image2": 1, "Image3": 2, "Image4": 3}
 
-        for mode in ComponentViewMode:
-            self.first_image_mode_compo_box.addItem(mode.name.capitalize())
-            self.second_image_mode_compo_box.addItem(mode.name.capitalize())
+        image_indices = {
+            "output_1": [
+                img_idx_dictionary[self.window.image_1_output_1.currentText()],
+                img_idx_dictionary[self.window.image_2_output_1.currentText()],
+            ],
+            "output_2": [
+                img_idx_dictionary[self.window.image_1_output_2.currentText()],
+                img_idx_dictionary[self.window.image_2_output_2.currentText()],
+            ],
+        }
 
-        for image in ImageNumber:
-            self.first_image_combo_box.addItem(image.name.capitalize())
-            self.second_image_combo_box.addItem(image.name.capitalize())
+        images = {
+            "output_1": [
+                self.window.images[image_indices["output_1"][0]].image,
+                self.window.images[image_indices["output_1"][1]].image,
+            ],
+            "output_2": [
+                self.window.images[image_indices["output_2"][0]].image,
+                self.window.images[image_indices["output_2"][1]].image,
+            ],
+        }
 
-        self._initialize_slots()
+        self.window.image_output_1.clear()
+        self.window.image_output_2.clear()
 
-    def _initialize_slots(self) -> None:
-        self.component_1_weight_slider.valueChanged.connect(
-            self._component_1_weight_slider_value_changed
-        )
-        self.component_2_weight_slider.valueChanged.connect(
-            self._component_2_weight_slider_value_changed
-        )
+        for output in ["output_1", "output_2"]:
+            if all(image is not None for image in images[output]):
+                self._process_images(output, images[output])
 
-    def _component_1_weight_slider_value_changed(self, value) -> None:
-        print("slider 1 value =", value)
-        self.weight_1 = value / 10
+    def _process_images(self, output, images):
+        output_idx = int(output.split("_")[-1]) - 1
+        component_1 = self.window.__getattribute__(
+            f"image_1_component_{output}"
+        ).currentText()
+        component_2 = self.window.__getattribute__(
+            f"image_2_component_{output}"
+        ).currentText()
 
-    def _component_2_weight_slider_value_changed(self, value) -> None:
-        print("slider 2 value =", value)
-        self.weight_2 = value / 10
-
-    def reconstruct_image_using_real_imaginary(
-        self, image_1: Image, image_2: Image, region: tuple
-    ):
-        sliced_image_1_real = image_1.real[region[0] : region[1], region[2] : region[3]]
-        print(region)
-        print(sliced_image_1_real.shape)
-        sliced_image_2_imaginary = image_2.imaginary[
-            region[0] : region[1], region[2] : region[3]
-        ]
-        combined_complex = (
-            self.weight_1 * sliced_image_1_real
-            + 1j * self.weight_2 * sliced_image_2_imaginary
-        )
-
-        # Perform the inverse Fourier Transform
-        reconstructed_image_complex = np.fft.ifft2(combined_complex)
-
-        # Take the absolute value to obtain the magnitude
-        reconstructed_magnitude = np.abs(reconstructed_image_complex)
-
-        # Normalize the reconstructed magnitude to the range [0, 255]
-        reconstructed_image = cv2.normalize(
-            reconstructed_magnitude, None, 0, 255, cv2.NORM_MINMAX
-        )
-
-        self.output_viewer.addItem(pg.ImageItem(reconstructed_image))
-
-    def reconstruct_image_using_magnitude_phase(
-        self, image_1: Image, image_2: Image, region: tuple
-    ):
-        # Slice the magnitude of image_1 to match the region
-        sliced_image_1_magnitude = image_1.magnitude[
-            region[0] : region[1], region[2] : region[3]
-        ]
-        sliced_image_2_phase = image_2.phase[
-            region[0] : region[1], region[2] : region[3]
-        ]
-
-        # Convert polar coordinates to Cartesian coordinates
-        real_part = (
-            self.weight_1 * sliced_image_1_magnitude * np.cos(sliced_image_2_phase)
-        )
-
-        imaginary_part = (
-            self.weight_2 * sliced_image_1_magnitude * np.sin(sliced_image_2_phase)
-        )
-
-        # Combine real and imaginary parts to form the complex image
-        complex_image = cv2.merge([real_part, imaginary_part])
-
-        # Perform Inverse Fourier Transform to reconstruct the image
-        reconstructed_image = cv2.idft(complex_image)
-
-        # Extract the real part of the reconstructed image
-        reconstructed_image = cv2.magnitude(
-            reconstructed_image[:, :, 0], reconstructed_image[:, :, 1]
-        )
-
-        # Normalize the reconstructed image to the range [0, 255]
-        reconstructed_image = cv2.normalize(
-            reconstructed_image, None, 0, 255, cv2.NORM_MINMAX
-        )
-
-        # Convert the reconstructed image to unsigned 8-bit integer (uint8)
-        reconstructed_image = np.uint8(reconstructed_image)
-
-        self.output_viewer.addItem(pg.ImageItem(reconstructed_image))
+        if component_1 == "Magnitude" and component_2 == "Phase":
+            self.window.output_ports[
+                output_idx
+            ].reconstruct_image_using_magnitude_phase(*images, region=self.region)
+        elif component_1 == "Phase" and component_2 == "Magnitude":
+            self.window.output_ports[
+                output_idx
+            ].reconstruct_image_using_magnitude_phase(
+                *reversed(images), region=self.region
+            )
+        elif component_1 == "Real" and component_2 == "Imaginary":
+            self.window.output_ports[output_idx].reconstruct_image_using_real_imaginary(
+                *images, region=self.region
+            )
+        elif component_1 == "Imaginary" and component_2 == "Real":
+            self.window.output_ports[output_idx].reconstruct_image_using_real_imaginary(
+                *reversed(images), region=self.region
+            )
